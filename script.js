9a92a963b51c972788238008b9ff3a0b4ea024f3
weslley <weslleycardoso@gmail.com>
2024-05-01 23:47:31 -0300

    Criando Script do Slide show

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ const header = document.querySelector("header");
 window.addEventListener ("sroll", function(){
     header.classList.toggle ("sticky", this.window.scrollY >0);
 })
+
+
+/* Comando dos slide show */
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+let slides = document.getElementsByClassName("slide");
+for (let i = 0; i < slides.length; i++) {
+slides[i].style.display = "none";
+}
+slideIndex++;
+if (slideIndex > slides.length) {slideIndex = 1}
+slides[slideIndex-1].style.display = "block";
+setTimeout(showSlides, 2000); // Altere o valor para ajustar a velocidade do slideshow (em milissegundos)
+}
