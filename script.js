bc17ce27da4ed82632d2b462a4ab6736bf30e0ed
weslley <weslleycardoso011@gmail.com>
2024-05-02 23:46:50 -0300

    Criando um Exemplo do jogo quiz

--- script.js
+++ script.js
@@ -74,3 +74,25 @@ form.addEventListener("submit", (event) =>{
 });
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+// TUDO SOBRE O QUIZ  
+//declarando variaveis
+
+let p1=prompt("Digite seu nome");
+let p2=prompt("Digite seu poder");
+let p3=prompt("Digite o nome de um vilao");
+let p4=prompt("Digite um lugar");
+let msg= document.getElementById("msg");
+
+msg.innerHTML = `Ola ${p1}, o seu poder e ${p2} e voce vai enfrentar o ${p3} no/em ${p4}`
